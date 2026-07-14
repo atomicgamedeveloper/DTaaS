95ef9e022804cd1c8e553eaa1797573660305443
atomic <enokmalik@gmail.com>
2026-07-14 06:22:38 +0200

    Basic Vite integration

---
+++ client/vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+
+export default defineConfig({
+    plugins: [react(), tsconfigPaths()],
+    server: {
+        host: '0.0.0.0',
+        port: 4000,
+        open: true,
+        strictPort: true,
+    },
+    preview: {
+        host: '0.0.0.0',
+        port: 4000,
+        strictPort: true,
+        open: true,
+    },
+    build: {
+        outDir: 'build',
+    }
+});
