861799bb08588e23bb908901d255cc41450eae9a
atomic <enokmalik@gmail.com>
2026-07-14 08:21:18 +0200

    Formatting issues

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,22 +2,21 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-
 export default defineConfig({
-    plugins: [react(), tsconfigPaths()],
-    server: {
-        host: '0.0.0.0',
-        port: 4000,
-        open: true,
-        strictPort: true,
-    },
-    preview: {
-        host: '0.0.0.0',
-        port: 4000,
-        strictPort: true,
-        open: true,
-    },
-    build: {
-        outDir: 'build',
-    }
+  plugins: [react(), tsconfigPaths()],
+  server: {
+    host: '0.0.0.0',
+    port: 4000,
+    open: true,
+    strictPort: true,
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 4000,
+    strictPort: true,
+    open: true,
+  },
+  build: {
+    outDir: 'build',
+  },
 });
